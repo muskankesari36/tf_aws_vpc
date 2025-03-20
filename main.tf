ff02cac3944335008c5c6ddaade77d6fe2e80801
Muskankesarwani-erm <TrustedAdvisor@INGNONB65754.insight.com>
2025-03-20 15:35:11 +0530

    removing backend config

--- main.tf
+++ main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "mystatebucket90"
-    key            = "s3-bucket/tf.state"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "tf-state-lock"
-
-  }
-}
 
 data "aws_s3_bucket" "s3_log_bucket" {
 
